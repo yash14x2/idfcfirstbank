a7d40519270d93fa44e5d7fc506de1ac4c177378
Vishal Igave <vishal.igave@teknopoint.in>
2023-09-05 17:37:02 +0530

    remove code

--- scripts/custom-script.js
+++
@@ -1,6 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function () {
-    const firstUl = document.querySelector('.nav-sections > ul:first-child');
-    if (firstUl) {
-        firstUl.classList.add('nav-main');
-      }
-// });
