aee5ea888c8a402fa256d16304a9163fc5b99896
Vishal Igave <vishal.igave@teknopoint.in>
2023-09-05 17:35:35 +0530

    code commented

--- scripts/custom-script.js
+++ scripts/custom-script.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
     const firstUl = document.querySelector('.nav-sections > ul:first-child');
     if (firstUl) {
         firstUl.classList.add('nav-main');
       }
-});
+// });
