4b730aed464ef861040f61dbde539486627883cb
Vishal Igave <vishal.igave@teknopoint.in>
2023-09-07 13:07:56 +0530

    js changes added

--- scripts/custom-script.js
+++ scripts/custom-script.js
@@ -37,7 +37,9 @@ function checkScroll() {
 // Add a scroll event listener to trigger the initialization when the user scrolls
 window.addEventListener('scroll', checkScroll);
 
-var liElem = document.querySelector('.secondary-nav-section-wrapper .default-content-wrapper ul li');
-liElem.forEach(function(item) {
-    item.classList.add('up-arrow'); 
-  });
+document.addEventListener("DOMContentLoaded", function () {
+    var liElem = document.querySelector('.secondary-nav-section-wrapper .default-content-wrapper ul li');
+    liElem.forEach(function(item) {
+        item.classList.add('up-arrow'); 
+    });
+});
