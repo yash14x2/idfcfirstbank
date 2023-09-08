d8f200f037c45a0032b954efb103c0437d06c2c4
yash14x2 <48061176+yash14x2@users.noreply.github.com>
2023-09-08 17:13:03 +0530

    Update custom-script.js

--- scripts/custom-script.js
+++ scripts/custom-script.js
@@ -2,42 +2,37 @@ let sliderInitialized = false; // To track whether the slider has been initializ
 
 // Function to initialize the slider
 function initializeSlider() {
+  const slider = tns({
+    container: '.hero-section-wrapper',
+    loop: false,
+    items: 1,
+    slideBy: 'page',
+    nav: false,
+    autoplay: false,
+    speed: 400,
+    autoplayButtonOutput: false,
+    controls: true,
+    mouseDrag: true,
+    lazyload: true,
+  });
 
-    const slider = tns({
-      container: '.hero-section-wrapper',
-      loop: false,
-      items: 1,
-      slideBy: 'page',
-      nav: false,
-      autoplay: false,
-      speed: 400,
-      autoplayButtonOutput: false,
-      controls: true,
-      mouseDrag: true,
-      lazyload: true,
-    });
+  sliderInitialized = true; // Set to true to avoid reinitialization
 
-    sliderInitialized = true; // Set to true to avoid reinitialization
-  }
+  // Select the button element with the data-controls attribute set to "next"
+  const Nbutton = document.querySelector('button[data-controls="next"]');
 
+  // Change the text inside the button
+  Nbutton.textContent = " "; // Replace "New Text" with your desired text
 
-// Function to check if the user has scrolled to a certain point
-function checkScroll() {
-  const triggerPoint = 10;
+  const Pbutton = document.querySelector('button[data-controls="prev"]');
 
-
-  if (window.scrollY > triggerPoint) {
-    initializeSlider();
-    // Remove the scroll event listener to prevent reinitialization
-    window.removeEventListener('scroll', checkScroll);
-  }
+  // Change the text inside the button
+  Pbutton.textContent = " "; // Replace "New Text" with your desired text
 }
 
-// Select the button element with the data-controls attribute set to "next"
+// Delay the initialization of the slider for 2 seconds after the page loads
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(initializeSlider, 2000); // 2000 milliseconds (2 seconds)
+});
 
-
-
-
-
-// Add a scroll event listener to trigger the initialization when the user scrolls
-window.addEventListener('scroll', checkScroll);
+// You can add more code here to work with the button element if needed
