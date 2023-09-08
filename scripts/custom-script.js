e8d7249884a50a68d95b73f947cca6dd80360e4f
yash14x2 <48061176+yash14x2@users.noreply.github.com>
2023-09-08 18:59:38 +0530

    Update custom-script.js

--- scripts/custom-script.js
+++ scripts/custom-script.js
@@ -36,3 +36,51 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // You can add more code here to work with the button element if needed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let sliderInitializedd = false; // To track whether the slider has been initialized
+
+// Function to initialize the slider
+function initializeSliderr() {
+  const slider = tns({
+    container: '.hero-section-wrapper',
+    loop: false,
+    items: 1,
+    slideBy: 'page',
+    nav: false,
+    autoplay: false,
+    speed: 400,
+    autoplayButtonOutput: false,
+    controls: true,
+    mouseDrag: true,
+    lazyload: true,
+  });
+
+  sliderInitializedd = true; // Set to true to avoid reinitialization
+
+
+}
+
+// Delay the initialization of the slider for 2 seconds after the page loads
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(initializeSliderr, 2000); // 2000 milliseconds (2 seconds)
+});
+
+// You can add more code here to work with the button element if needed
