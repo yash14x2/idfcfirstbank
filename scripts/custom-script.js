22b326328c6a4fb562d07a14a1d00279e4e0601a
yash14x2 <48061176+yash14x2@users.noreply.github.com>
2023-09-12 16:13:05 +0530

    Update custom-script.js

--- scripts/custom-script.js
+++ scripts/custom-script.js
@@ -22,7 +22,7 @@ function initializeSlider() {
   });
 
   const slider2 = tns({
-    container: '.bank-card-slider-wapper .cards-wrapper .cards ul',
+    container: '.bank-card-slider-wapper .cards-wrapper .cards block',
     loop: false,
     items: 3,
     slideBy: 'page',
