e99cc38198a54eede558f5ffe1a98f81bf912b22
Vishal Igave <vishal.igave@teknopoint.in>
2023-09-12 16:45:03 +0530

    css changes added

--- scripts/custom-script.js
+++ scripts/custom-script.js
@@ -25,7 +25,7 @@ function initializeSlider() {
     container: '.bank-card-slider-wapper .cards-wrapper .cards',
 
     loop: false,
-    items: 3.5,
+    items: 3.2,
     slideBy: 'page',
     nav: false,
     autoplay: false,
