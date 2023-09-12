e6e24b3588ea027f29988e8a88afd26597421ca6
yash14x2 <48061176+yash14x2@users.noreply.github.com>
2023-09-12 16:09:04 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,84 +1,135 @@
-let sliderInitialized = false; // To track whether the slider has been initialized
+import {
+  sampleRUM,
+  buildBlock,
+  loadHeader,
+  loadFooter,
+  decorateButtons,
+  decorateIcons,
+  decorateSections,
+  decorateBlocks,
+  decorateTemplateAndTheme,
+  waitForLCP,
+  loadBlocks,
+  loadCSS,
+} from './lib-franklin.js';
 
-// Function to initialize the slider
-function initializeSlider() {
-  const slider = tns({
-    container: '.hero-section-wrapper',
-    loop: false,
-    items: 1,
-    slideBy: 'page',
-    nav: false,
-    autoplay: false,
-    speed: 400,
-    autoplayButtonOutput: false,
-    controls: true,
-    mouseDrag: true,
-    lazyload: true,
-    responsive: { 
-      768: {
-          items: 1,
-      }
+const LCP_BLOCKS = []; // add your LCP blocks to the list
+
+/**
+ * Builds hero block and prepends to main in a new section.
+ * @param {Element} main The container element
+ */
+function buildHeroBlock(main) {
+  const h1 = main.querySelector('h1');
+  const picture = main.querySelector('picture');
+  // eslint-disable-next-line no-bitwise
+  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+    const section = document.createElement('div');
+    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    main.prepend(section);
   }
-  });
-
-  // const slider2 = tns({
-  //   container: '.bank-card-slider-wapper .cards-wrapper .cards ul',
-  //   loop: false,
-  //   items: 3,
-  //   slideBy: 'page',
-  //   nav: false,
-  //   autoplay: false,
-  //   speed: 400,
-  //   autoplayButtonOutput: false,
-  //   controls: true,
-  //   mouseDrag: true,
-  //   lazyload: true,
-  //   responsive: { 
-  //     // 768: {
-  //     //     items: 1,
-  //     // }
-  // }
-  // });
-
-  sliderInitialized = true; // Set to true to avoid reinitialization
-
-  // Select the button element with the data-controls attribute set to "next"
-  const Nbutton = document.querySelector('button[data-controls="next"]');
-
-  // Change the text inside the button
-  Nbutton.textContent = " "; // Replace "New Text" with your desired text
-
-  const Pbutton = document.querySelector('button[data-controls="prev"]');
-
-  // Change the text inside the button
-  Pbutton.textContent = " "; // Replace "New Text" with your desired text
 }
-function navActiveTab(){
-  var active = document.querySelector('.nav-sections ul li:first-child');
-  active.classList.add('active-tab');
+
+/**
+ * load fonts.css and set a session storage flag
+ */
+async function loadFonts() {
+  await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
+  try {
+    if (!window.location.hostname.includes('localhost')) sessionStorage.setItem('fonts-loaded', 'true');
+  } catch (e) {
+    // do nothing
+  }
 }
-// Delay the initialization of the slider for 2 seconds after the page loads
-document.addEventListener('DOMContentLoaded', function() {
-  setTimeout(
-    initializeSlider,
-     2000); // 2000 milliseconds (2 seconds)
-});
 
-// You can add more code here to work with the button element if needed
+/**
+ * Builds all synthetic blocks in a container element.
+ * @param {Element} main The container element
+ */
+function buildAutoBlocks(main) {
+  try {
+    buildHeroBlock(main);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Auto Blocking failed', error);
+  }
+}
 
+/**
+ * Decorates the main element.
+ * @param {Element} main The main element
+ */
+// eslint-disable-next-line import/prefer-default-export
+export function decorateMain(main) {
+  // hopefully forward compatible button decoration
+  decorateButtons(main);
+  decorateIcons(main);
+  buildAutoBlocks(main);
+  decorateSections(main);
+  decorateBlocks(main);
+}
 
+/**
+ * Loads everything needed to get to LCP.
+ * @param {Element} doc The container element
+ */
+async function loadEager(doc) {
+  document.documentElement.lang = 'en';
+  decorateTemplateAndTheme();
+  const main = doc.querySelector('main');
+  if (main) {
+    decorateMain(main);
+    document.body.classList.add('appear');
+    await waitForLCP(LCP_BLOCKS);
+  }
 
+  try {
+    /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
+    if (window.innerWidth >= 900 || sessionStorage.getItem('fonts-loaded')) {
+      loadFonts();
+    }
+  } catch (e) {
+    // do nothing
+  }
+}
 
+/**
+ * Loads everything that doesn't need to be delayed.
+ * @param {Element} doc The container element
+ */
+async function loadLazy(doc) {
+  const main = doc.querySelector('main');
+  await loadBlocks(main);
 
+  const { hash } = window.location;
+  const element = hash ? doc.getElementById(hash.substring(1)) : false;
+  if (hash && element) element.scrollIntoView();
 
+  loadHeader(doc.querySelector('header'));
+  loadFooter(doc.querySelector('footer'));
 
+  loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
+  loadFonts();
 
+  sampleRUM('lazy');
+  sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
+  sampleRUM.observe(main.querySelectorAll('picture > img'));
+}
 
+/**
+ * Loads everything that happens a lot later,
+ * without impacting the user experience.
+ */
+function loadDelayed() {
+  // eslint-disable-next-line import/no-cycle
+  window.setTimeout(() => import('./delayed.js'), 3000);
+  // load anything that can be postponed to the latest here
+}
 
+async function loadPage() {
+  await loadEager(document);
+  await loadLazy(document);
+  loadDelayed();
+}
 
-
-
-
-
-
-
+loadPage();
