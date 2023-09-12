71b89e4aa26015a4160d0b31957facfc8a1bfeba
yash14x2 <48061176+yash14x2@users.noreply.github.com>
2023-09-12 17:38:44 +0530

    Update custom-script.js

--- scripts/custom-script.js
+++ scripts/custom-script.js
@@ -52,58 +52,58 @@ document.addEventListener('DOMContentLoaded', function() {
      2000); // 2000 milliseconds (2 seconds)
 });
 
-function initializeSlider2() {
-    const slider = tns({
-      container: '.bank-card-slider-wapper .cards-wrapper .cards ul',
-      loop: false,
-      items: 2,
-      slideBy: 'page',
-      nav: false,
-      autoplay: false,
-      speed: 400,
-      autoplayButtonOutput: false,
-      controls: true,
-      mouseDrag: true,
-      lazyload: true,
-      responsive: { 
-        768: {
-          items: 1,
-        }
-      }
-    });
+// function initializeSlider2() {
+//     const slider = tns({
+//       container: '.bank-card-slider-wapper .cards-wrapper .cards ul',
+//       loop: false,
+//       items: 2,
+//       slideBy: 'page',
+//       nav: false,
+//       autoplay: false,
+//       speed: 400,
+//       autoplayButtonOutput: false,
+//       controls: true,
+//       mouseDrag: true,
+//       lazyload: true,
+//       responsive: { 
+//         768: {
+//           items: 1,
+//         }
+//       }
+//     });
   
-    sliderInitialized = true; // Set to true to avoid reinitialization
+//     sliderInitialized = true; // Set to true to avoid reinitialization
   
-    const Nbutton = document.querySelector('button[data-controls="next"]');
-    Nbutton.textContent = " "; // Replace "New Text" with your desired text
+//     const Nbutton = document.querySelector('button[data-controls="next"]');
+//     Nbutton.textContent = " "; // Replace "New Text" with your desired text
   
-    const Pbutton = document.querySelector('button[data-controls="prev"]');
-    Pbutton.textContent = " "; // Replace "New Text" with your desired text
-  }
+//     const Pbutton = document.querySelector('button[data-controls="prev"]');
+//     Pbutton.textContent = " "; // Replace "New Text" with your desired text
+//   }
   
-  function onElementIntersection(entries, observer) {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        initializeSlider2(); // Initialize the slider when the element is in view
-        observer.unobserve(entry.target); // Stop observing once it's initialized
-      }
-    });
-  }
+//   function onElementIntersection(entries, observer) {
+//     entries.forEach(entry => {
+//       if (entry.isIntersecting) {
+//         initializeSlider2(); // Initialize the slider when the element is in view
+//         observer.unobserve(entry.target); // Stop observing once it's initialized
+//       }
+//     });
+//   }
   
-  // Specify the element you want to watch for visibility
-  const targetElement = document.querySelector('.bank-card-slider-wapper .cards-wrapper .cards');
+//   // Specify the element you want to watch for visibility
+//   const targetElement = document.querySelector('.bank-card-slider-wapper .cards-wrapper .cards');
   
-  const observerOptions = {
-    root: null, // Use the viewport as the root
-    rootMargin: '0px', // No margin
-    threshold: 0.5, // Trigger when at least 50% of the element is in view
-  };
+//   const observerOptions = {
+//     root: null, // Use the viewport as the root
+//     rootMargin: '0px', // No margin
+//     threshold: 0.5, // Trigger when at least 50% of the element is in view
+//   };
   
-  const intersectionObserver = new IntersectionObserver(onElementIntersection, observerOptions);
+//   const intersectionObserver = new IntersectionObserver(onElementIntersection, observerOptions);
   
-  document.addEventListener('DOMContentLoaded', function() {
-    intersectionObserver.observe(targetElement); // Start observing the target element
-  });
+//   document.addEventListener('DOMContentLoaded', function() {
+//     intersectionObserver.observe(targetElement); // Start observing the target element
+//   });
   
 
   
