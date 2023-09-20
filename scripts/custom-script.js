0b60e7f24aaed7b36ce0f1ef3dd4fd9de60f4e1a
yash14x2 <48061176+yash14x2@users.noreply.github.com>
2023-09-20 16:03:10 +0530

    Update custom-script.js

--- scripts/custom-script.js
+++ scripts/custom-script.js
@@ -38,22 +38,22 @@ function initializeSlider() {
 
 
 
-function siemafun() {
-  new Siema({
-    selector: '.get-more-from-bank-wapper .cards-wrapper .block ul',
-    duration: 200,
-    easing: 'ease-out',
-    perPage: 1,
-    startIndex: 0,
-    draggable: true,
-    multipleDrag: true,
-    threshold: 20,
-    loop: false,
-    rtl: false,
-    onInit: () => {},
-    onChange: () => {},
-  });
-}
+// function siemafun() {
+//   new Siema({
+//     selector: '.get-more-from-bank-wapper .cards-wrapper .block ul',
+//     duration: 200,
+//     easing: 'ease-out',
+//     perPage: 1,
+//     startIndex: 0,
+//     draggable: true,
+//     multipleDrag: true,
+//     threshold: 20,
+//     loop: false,
+//     rtl: false,
+//     onInit: () => {},
+//     onChange: () => {},
+//   });
+// }
 
 
 
