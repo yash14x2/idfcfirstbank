50a007e80ac2a5d04c3f3217372c5fd5f08d3e0d
yash14x2 <48061176+yash14x2@users.noreply.github.com>
2023-09-20 19:19:54 +0530

    Update custom-script.js

--- scripts/custom-script.js
+++ scripts/custom-script.js
@@ -43,7 +43,7 @@ function siemafun() {
     selector: '.get-more-from-bank-wapper .cards-wrapper .block ul',
     duration: 200,
     easing: 'ease-out',
-    perPage: 1,
+    perPage: 4,
     startIndex: 0,
     draggable: true,
     multipleDrag: true,
