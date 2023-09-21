bb5b93831b1a66e4d46010f7230b59f24d6f67e6
yash14x2 <48061176+yash14x2@users.noreply.github.com>
2023-09-21 13:29:49 +0530

    Update custom-script.js

--- scripts/custom-script.js
+++ scripts/custom-script.js
@@ -1,5 +1,5 @@
+let sliderInitialized = false; // To track whether the slider has been initialized
 
-let sliderInitialized = false;
 // Function to initialize the slider
 function initializeSlider() {
   const slider = tns({
@@ -14,44 +14,59 @@ function initializeSlider() {
     controls: true,
     mouseDrag: true,
     lazyload: true,
-    responsive: {
+    responsive: { 
       768: {
-        items: 1,
-
+          items: 1,
       }
-    }
+  }
   });
+
 
   sliderInitialized = true; // Set to true to avoid reinitialization
+
+  // Select the button element with the data-controls attribute set to "next"
+  const Nbutton = document.querySelector('button[data-controls="next"]');
+
+  // Change the text inside the button
+  Nbutton.textContent = " "; // Replace "New Text" with your desired text
+
+  const Pbutton = document.querySelector('button[data-controls="prev"]');
+
+  // Change the text inside the button
+  Pbutton.textContent = " "; // Replace "New Text" with your desired text
 }
 
-function siemafun() {
-  var slider = tns({
-    loop: false,
-    container: '.get-more-from-bank-wapper .cards-wrapper .block ul',
-    loop: false,
-    items: 1,
-    slideBy: 'page',
-    nav: false,
-    autoplay: false,
-    speed: 400,
-    autoplayButtonOutput: false,
-    controls: true,
-    mouseDrag: true,
-    lazyload: true,
-    responsive: {
-      768: {
-        items: 1,
 
-      }
+
+function siemafun() {
+var slider = tns({
+  loop: false,
+  container: '.get-more-from-bank-wapper .cards-wrapper .block ul',
+    
+  loop: false,
+  items: 1,
+  slideBy: 'page',
+  nav: false,
+  autoplay: false,
+  speed: 400,
+  autoplayButtonOutput: false,
+  controls: true,
+  mouseDrag: true,
+  lazyload: true,
+  responsive: { 
+    768: {
+        items: 1,
     }
-  });
+}
+});
+  
 }
 
 function siemafun2() {
   var slider2 = tns({
     loop: false,
     container: '.bank-card-slider-wapper .cards-wrapper .block ul',
+      
     loop: false,
     items: 1,
     slideBy: 'page',
@@ -62,23 +77,38 @@ function siemafun2() {
     controls: true,
     mouseDrag: true,
     lazyload: true,
-    responsive: {
+    responsive: { 
       768: {
-        items: 1,
-        
+          items: 1,
       }
-    }
+  }
   });
-}
+    
+  }
+  
 
+
+
+
+
+
+function navActiveTab(){
+  var active = document.querySelector('.nav-sections ul li:first-child');
+  active.classList.add('active-tab');
+}
 // Delay the initialization of the slider for 2 seconds after the page loads
 document.addEventListener('DOMContentLoaded', function() {
-  setTimeout(function() {
-    initializeSlider();
-  }, 2000);
+ setTimeout(function() {
+  initializeSlider();
+ 
+}, 2000);
 
-  setTimeout(function() {
-    siemafun();
-    siemafun2();
-  }, 4000);
+
+ setTimeout(function() {
+   siemafun();
+   siemafun2();
+ 
+}, 4000);
+  
+  // 2000 milliseconds (2 seconds)
 });
