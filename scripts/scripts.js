082bfa4d05aed841fed1119e656b53964a3468df
yash14x2 <48061176+yash14x2@users.noreply.github.com>
2023-09-25 12:38:40 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,7 @@
 import {
   sampleRUM,
   buildBlock,
+  getMetadata,
   loadHeader,
   loadFooter,
   decorateButtons,
@@ -11,34 +12,78 @@ import {
   waitForLCP,
   loadBlocks,
   loadCSS,
+  toClassName,
 } from './lib-franklin.js';
+import {
+  analyticsTrack404,
+  analyticsTrackConversion,
+  analyticsTrackCWV,
+  analyticsTrackError,
+  initAnalyticsTrackingQueue,
+  setupAnalyticsTrackingWithAlloy,
+} from './analytics/lib-analytics.js';
 
 const LCP_BLOCKS = []; // add your LCP blocks to the list
+window.hlx.RUM_GENERATION = 'project-1'; // add your RUM generation information here
+
+// Define the custom audiences mapping for experimentation
+const EXPERIMENTATION_CONFIG = {
+  audiences: {
+    device: {
+      mobile: () => window.innerWidth < 600,
+      desktop: () => window.innerWidth >= 600,
+    },
+    visitor: {
+      new: () => !localStorage.getItem('franklin-visitor-returning'),
+      returning: () => !!localStorage.getItem('franklin-visitor-returning'),
+    },
+  },
+};
 
 /**
- * Builds hero block and prepends to main in a new section.
- * @param {Element} main The container element
+ * Determine if we are serving content for the block-library, if so don't load the header or footer
+ * @returns {boolean} True if we are loading block library content
  */
+export function isBlockLibrary() {
+  return window.location.pathname.includes('block-library');
+}
+
+/**
+ * Convience method for creating tags in one line of code
+ * @param {string} tag Tag to create
+ * @param {object} attributes Key/value object of attributes
+ * @param {HTMLElement | HTMLElement[] | string} children Child element
+ * @returns {HTMLElement} The created tag
+ */
+export function createTag(tag, attributes, children) {
+  const element = document.createElement(tag);
+  if (children) {
+    if (children instanceof HTMLElement
+      || children instanceof SVGElement
+      || children instanceof DocumentFragment) {
+      element.append(children);
+    } else if (Array.isArray(children)) {
+      element.append(...children);
+    } else {
+      element.insertAdjacentHTML('beforeend', children);
+    }
+  }
+  if (attributes) {
+    Object.entries(attributes).forEach(([key, val]) => {
+      element.setAttribute(key, val);
+    });
+  }
+  return element;
+}
+
 function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
+  const h1 = main.querySelector('main > div > h1');
+  const picture = main.querySelector('main > div > p > picture');
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
     section.append(buildBlock('hero', { elems: [picture, h1] }));
     main.prepend(section);
-  }
-}
-
-/**
- * load fonts.css and set a session storage flag
- */
-async function loadFonts() {
-  await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
-  try {
-    if (!window.location.hostname.includes('localhost')) sessionStorage.setItem('fonts-loaded', 'true');
-  } catch (e) {
-    // do nothing
   }
 }
 
@@ -55,6 +100,59 @@ function buildAutoBlocks(main) {
   }
 }
 
+function patchDemoBlocks(config) {
+  if (window.wknd.demoConfig.blocks && window.wknd.demoConfig.blocks[config.blockName]) {
+    const url = window.wknd.demoConfig.blocks[config.blockName];
+    const splits = new URL(url).pathname.split('/');
+    const [, owner, repo, , branch] = splits;
+    const path = splits.slice(5).join('/');
+
+    const franklinPath = `https://little-forest-58aa.david8603.workers.dev/?url=https://${branch}--${repo}--${owner}.hlx.live/${path}`;
+    return {
+      ...config,
+      jsPath: `${franklinPath}/${config.blockName}.js`,
+      cssPath: `${franklinPath}/${config.blockName}.css`,
+    };
+  }
+  return (config);
+}
+
+async function loadDemoConfig() {
+  const demoConfig = {};
+  const pathSegments = window.location.pathname.split('/');
+  if (window.location.pathname.startsWith('/drafts/') && pathSegments.length > 4) {
+    const demoBase = pathSegments.slice(0, 4).join('/');
+    const resp = await fetch(`${demoBase}/theme.json?sheet=default&sheet=blocks&`);
+    if (resp.status === 200) {
+      const json = await resp.json();
+      const tokens = json.data || json.default.data;
+      const root = document.querySelector(':root');
+      tokens.forEach((e) => {
+        root.style.setProperty(`--${e.token}`, `${e.value}`);
+        demoConfig[e.token] = e.value;
+      });
+      demoConfig.tokens = tokens;
+      demoConfig.demoBase = demoBase;
+      const blocks = json.blocks ? json.blocks.data : [];
+      demoConfig.blocks = {};
+      blocks.forEach((block) => {
+        demoConfig.blocks[block.name] = block.url;
+      });
+
+      window.hlx.patchBlockConfig.push(patchDemoBlocks);
+    }
+
+    if (!demoConfig.demoBase) {
+      const navCheck = await fetch(`${demoBase}/nav.plain.html`);
+      if (navCheck.status === 200) {
+        demoConfig.demoBase = demoBase;
+      }
+    }
+  }
+  window.wknd = window.wknd || {};
+  window.wknd.demoConfig = demoConfig;
+}
+
 /**
  * Decorates the main element.
  * @param {Element} main The main element
@@ -64,61 +162,112 @@ export function decorateMain(main) {
   // hopefully forward compatible button decoration
   decorateButtons(main);
   decorateIcons(main);
+
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
 }
 
 /**
- * Loads everything needed to get to LCP.
- * @param {Element} doc The container element
+ * loads everything needed to get to LCP.
  */
 async function loadEager(doc) {
   document.documentElement.lang = 'en';
   decorateTemplateAndTheme();
-  const main = doc.querySelector('main');
-  if (main) {
-    decorateMain(main);
-    document.body.classList.add('appear');
-    await waitForLCP(LCP_BLOCKS);
+
+  // load experiments
+  const experiment = toClassName(getMetadata('experiment'));
+  const instantExperiment = getMetadata('instant-experiment');
+  if (instantExperiment || experiment) {
+    const { runExperiment } = await import('./experimentation/index.js');
+    await runExperiment(experiment, instantExperiment, EXPERIMENTATION_CONFIG);
   }
 
-  try {
-    /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
-    if (window.innerWidth >= 900 || sessionStorage.getItem('fonts-loaded')) {
-      loadFonts();
-    }
-  } catch (e) {
-    // do nothing
+  // load demo config
+  await loadDemoConfig();
+
+  const main = doc.querySelector('main');
+  if (main) {
+    await initAnalyticsTrackingQueue();
+    decorateMain(main);
+    await waitForLCP(LCP_BLOCKS);
   }
 }
 
 /**
- * Loads everything that doesn't need to be delayed.
- * @param {Element} doc The container element
+ * Adds the favicon.
+ * @param {string} href The favicon URL
+ */
+export function addFavIcon(href) {
+  const link = document.createElement('link');
+  link.rel = 'icon';
+  link.type = 'image/png';
+  link.href = href;
+  const existingLink = document.querySelector('head link[rel="icon"]');
+  if (existingLink) {
+    existingLink.parentElement.replaceChild(link, existingLink);
+  } else {
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+}
+
+/**
+ * loads everything that doesn't need to be delayed.
  */
 async function loadLazy(doc) {
   const main = doc.querySelector('main');
   await loadBlocks(main);
 
   const { hash } = window.location;
-  const element = hash ? doc.getElementById(hash.substring(1)) : false;
+  const element = hash ? main.querySelector(hash) : false;
   if (hash && element) element.scrollIntoView();
 
-  loadHeader(doc.querySelector('header'));
-  loadFooter(doc.querySelector('footer'));
+  if (!isBlockLibrary()) {
+    loadHeader(doc.querySelector('header'));
+    loadFooter(doc.querySelector('footer'));
+  }
 
-  loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  loadFonts();
-
+  if (window.wknd.demoConfig.fonts) {
+    const fonts = window.wknd.demoConfig.fonts.split('\n');
+    fonts.forEach(async (font) => {
+      const [family, url] = font.split(': ');
+      const ff = new FontFace(family, `url('${url}')`);
+      await ff.load();
+      document.fonts.add(ff);
+    });
+  } else {
+    loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
+  }
+  addFavIcon(`${window.wknd.demoConfig.demoBase || window.hlx.codeBasePath}/favicon.png`);
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
+
+  // Load experimentation preview overlay
+  if (window.location.hostname === 'localhost' || window.location.hostname.endsWith('.hlx.page')) {
+    const preview = await import(`${window.hlx.codeBasePath}/tools/preview/preview.js`);
+    await preview.default();
+    if (window.hlx.experiment) {
+      const experimentation = await import(`${window.hlx.codeBasePath}/tools/preview/experimentation.js`);
+      experimentation.default();
+    }
+  }
+
+  // Mark customer as having viewed the page once
+  localStorage.setItem('franklin-visitor-returning', true);
+
+  const context = {
+    getMetadata,
+    toClassName,
+  };
+  // eslint-disable-next-line import/no-relative-packages
+  const { initConversionTracking } = await import('../plugins/rum-conversion/src/index.js');
+  await initConversionTracking.call(context, document);
 }
 
 /**
- * Loads everything that happens a lot later,
- * without impacting the user experience.
+ * loads everything that happens a lot later, without impacting
+ * the user experience.
  */
 function loadDelayed() {
   // eslint-disable-next-line import/no-cycle
@@ -129,7 +278,75 @@ function loadDelayed() {
 async function loadPage() {
   await loadEager(document);
   await loadLazy(document);
+  const setupAnalytics = setupAnalyticsTrackingWithAlloy(document);
   loadDelayed();
+  await setupAnalytics;
 }
+
+const cwv = {};
+
+// Forward the RUM CWV cached measurements to edge using WebSDK before the page unloads
+window.addEventListener('beforeunload', () => {
+  if (!Object.keys(cwv).length) return;
+  analyticsTrackCWV(cwv);
+});
+
+// Callback to RUM CWV checkpoint in order to cache the measurements
+sampleRUM.always.on('cwv', async (data) => {
+  if (!data.cwv) return;
+  Object.assign(cwv, data.cwv);
+});
+
+sampleRUM.always.on('404', analyticsTrack404);
+sampleRUM.always.on('error', analyticsTrackError);
+
+// Declare conversionEvent, bufferTimeoutId and tempConversionEvent,
+// outside the convert function to persist them for buffering between
+// subsequent convert calls
+const CONVERSION_EVENT_TIMEOUT_MS = 100;
+let bufferTimeoutId;
+let conversionEvent;
+let tempConversionEvent;
+sampleRUM.always.on('convert', (data) => {
+  const { element } = data;
+  // eslint-disable-next-line no-undef
+  if (!element || !alloy) {
+    return;
+  }
+
+  if (element.tagName === 'FORM') {
+    conversionEvent = {
+      ...data,
+      event: 'Form Complete',
+    };
+
+    if (conversionEvent.event === 'Form Complete'
+      // Check for undefined, since target can contain value 0 as well, which is falsy
+      && (data.target === undefined || data.source === undefined)
+    ) {
+      // If a buffer has already been set and tempConversionEvent exists,
+      // merge the two conversionEvent objects to send to alloy
+      if (bufferTimeoutId && tempConversionEvent) {
+        conversionEvent = { ...tempConversionEvent, ...conversionEvent };
+      } else {
+        // Temporarily hold the conversionEvent object until the timeout is complete
+        tempConversionEvent = { ...conversionEvent };
+
+        // If there is partial form conversion data,
+        // set the timeout buffer to wait for additional data
+        bufferTimeoutId = setTimeout(async () => {
+          analyticsTrackConversion({ ...conversionEvent });
+          tempConversionEvent = undefined;
+          conversionEvent = undefined;
+        }, CONVERSION_EVENT_TIMEOUT_MS);
+      }
+    }
+    return;
+  }
+
+  analyticsTrackConversion({ ...data });
+  tempConversionEvent = undefined;
+  conversionEvent = undefined;
+});
 
 loadPage();
