6b2fc0675c670d948db6ace4091e81813ffbb9f6
yash14x2 <48061176+yash14x2@users.noreply.github.com>
2023-10-03 12:41:15 +0530

    Update custom-script.js

--- scripts/custom-script.js
+++ scripts/custom-script.js
@@ -1,46 +1,46 @@
 let sliderInitialized = false; // To track whether the slider has been initialized
 
 // Function to initialize the slider
-// function initializeSlider() {
-//   if (!sliderInitialized) {
-//     const slider = tns({
-//       container: '.hero-section-wrapper',
-//       loop: false,
-//       items: 1,
-//       slideBy: 'page',
-//       nav: false,
-//       autoplay: false,
-//       speed: 400,
-//       autoplayButtonOutput: false,
-//       controls: true,
-//       mouseDrag: true,
-//       lazyload: true,
-//       responsive: {
-//         768: {
-//           items: 1,
-//         },
-//       },
-//     });
+function initializeSlider() {
+  if (!sliderInitialized) {
+    const slider = tns({
+      container: '.hero-section-wrapper',
+      loop: false,
+      items: 1,
+      slideBy: 'page',
+      nav: false,
+      autoplay: false,
+      speed: 400,
+      autoplayButtonOutput: false,
+      controls: true,
+      mouseDrag: true,
+      lazyload: true,
+      responsive: {
+        768: {
+          items: 1,
+        },
+      },
+    });
 
-//     sliderInitialized = true; // Set to true to avoid reinitialization
+    sliderInitialized = true; // Set to true to avoid reinitialization
 
-//     // Select the button element with the data-controls attribute set to "next"
-//     const Nbutton = document.querySelector('button[data-controls="next"]');
+    // Select the button element with the data-controls attribute set to "next"
+    const Nbutton = document.querySelector('button[data-controls="next"]');
 
-//     // Change the text inside the button
-//     Nbutton.textContent = " "; // Replace "New Text" with your desired text
+    // Change the text inside the button
+    Nbutton.textContent = " "; // Replace "New Text" with your desired text
 
-//     const Pbutton = document.querySelector('button[data-controls="prev"]');
+    const Pbutton = document.querySelector('button[data-controls="prev"]');
 
-//     // Change the text inside the button
-//     Pbutton.textContent = " "; // Replace "New Text" with your desired text
+    // Change the text inside the button
+    Pbutton.textContent = " "; // Replace "New Text" with your desired text
 
-//     // Show the slider container once it's initialized
-//     document.querySelector('.hero-section-wrapper').style.opacity = '1';
-//     document.querySelector('.hero-section-wrapper').style.position = 'relative';
-//     document.querySelector('.hero-section-wrapper').style.left = '0';
-//   }
-// }
+    // Show the slider container once it's initialized
+    document.querySelector('.hero-section-wrapper').style.opacity = '1';
+    document.querySelector('.hero-section-wrapper').style.position = 'relative';
+    document.querySelector('.hero-section-wrapper').style.left = '0';
+  }
+}
 
 
 function siemafun() {
@@ -125,10 +125,10 @@ function navActiveTab(){
 }
 // Delay the initialization of the slider for 2 seconds after the page loads
 document.addEventListener('DOMContentLoaded', function() {
-//  setTimeout(function() {
-//   initializeSlider();
+ setTimeout(function() {
+  initializeSlider();
  
-// }, 2000);
+}, 2000);
 
 
 
